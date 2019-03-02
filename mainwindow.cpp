64567543bb34e4428ad9981193896034990493a9
NessajHu <daobanqiao@163.com>
2019-03-02 21:19:27 +0800

    q

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -54,3 +54,36 @@ MainWindow::~MainWindow()
 {
     
 } 
+
+/*
+    if (sharedMemory.isAttached())
+        detach();
+
+    ui.label->setText(tr("Select an image file"));
+    QString fileName = QFileDialog::getOpenFileName(0, QString(), QString(),
+                                        tr("Images (*.png *.xpm *.jpg)"));
+    QImage image;
+    if (!image.load(fileName)) {
+        ui.label->setText(tr("Selected file is not an image, please select another."));
+        return;
+    }
+    ui.label->setPixmap(QPixmap::fromImage(image));
+//! [1] //! [2]
+
+    // load into shared memory
+    QBuffer buffer;
+    buffer.open(QBuffer::ReadWrite);
+    QDataStream out(&buffer);
+    out << image;
+    int size = buffer.size();
+
+    if (!sharedMemory.create(size)) {
+        ui.label->setText(tr("Unable to create shared memory segment."));
+        return;
+    }
+    sharedMemory.lock();
+    char *to = (char*)sharedMemory.data();
+    const char *from = buffer.data().data();
+    memcpy(to, from, qMin(sharedMemory.size(), size));
+    sharedMemory.unlock();
+*/
